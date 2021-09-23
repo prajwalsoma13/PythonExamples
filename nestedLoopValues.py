6cd29061b708992e11e3823cacd2dce753fcfb1a
PrajwalSomashekara13 <58339536+prajwalsoma13@users.noreply.github.com>
2021-09-23 15:40:01 +0530

    Add files via upload

---
+++ nestedLoopValues.py
@@ -0,0 +1,52 @@
+"""
+Author: Prajwal Somashekara
+Date: 23/09/2021 Thursday
+"""
+
+
+def getValues(dic):
+    variable = dic
+    TotalValue = 0
+    checkValue = True
+
+    while checkValue:
+        if isinstance(variable, dict):
+            for key in variable:
+                if variable[key] is None:
+                    checkValue = False
+                if isinstance(variable[key], int):
+                    TotalValue = TotalValue + variable[key]
+                    variable[key] = None
+
+                elif isinstance(variable[key], list):
+                    variable = variable[key]
+
+                elif isinstance(variable[key], dict):
+                    variable = variable[key]
+
+        elif isinstance(variable, list):
+            for keyList in variable:
+                variable = keyList
+
+    return TotalValue
+
+
+x = {
+    "value": 7,
+    "next": [
+        {"value": 13,
+         "next":
+             {
+                 "value": 4,
+                 "next": [
+                     {
+                         "value": 7
+                     }
+                 ]
+             }
+         }
+    ],
+}
+
+
+result = getValues(x)
